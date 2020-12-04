9d53b49b2826dd098c89f8ddd3c6b4df79d67c49
Berg Paulo <lindembergdepaulo@gmail.com>
2020-12-04 12:18:16 -0300

    Create app.py
    add app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import requests
+from random import randrange
+from flask import Flask, jsonify, g
+
+app = Flask(__name__)
+
+__url__ = "https://gist.githubusercontent.com/bergpb/76f23a2aab91ff09660ae1319e0b6f56/raw"
+
+@app.before_request
+def before_request_func():
+    frases = requests.get(__url__).text
+    list_frases = frases.split('\n')
+    g.frase = list_frases[randrange(len(list_frases))]
+
+
+@app.route('/', methods=['GET'])
+def index():
+    data = {
+        "success": True,
+        "frase": g.frase,
+        "autor": "Cardoso, Christian (Profanus)"
+    }
+    return jsonify(data = data)
